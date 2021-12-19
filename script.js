fc8ffa5cdacac445441327f96cc686b0475e4784
eli-levvin <88132546+eli-levvin@users.noreply.github.com>
2021-12-19 22:46:02 +0200

    w3d1 dom exercises

---
+++ script.js
@@ -0,0 +1,27 @@
+
+//first exercise
+
+/*const div = document.getElementById('navBar');
+div.setAttribute('id','socialNetworkNavigation');
+const li = document.createElement('li')
+const text = document.createTextNode('Logout')
+li.appendChild(text)
+const ul = document.querySelector('div ul')
+ul.appendChild(li)*/
+
+
+// second exercise 
+const div = document.querySelector('div')
+div.style.backgroundColor = 'lightblue'
+div.style.padding = '40px'
+
+const john = document.querySelector('ul')
+john.firstElementChild.style.display = 'none'
+
+const bodyFont = document.querySelector('body')
+bodyFont.style.fontSize == '50px'; //on the browser if i try to enter this statement it says false? 
+
+if(div.style.backgroundColor == 'lightblue'){
+alert('hello x and y') 
+}
+
